226be5f4c71ca6f97299f4b22ce11050e0b96a61
pyguru123 <pyguru123@gmail.com>
2021-03-13 00:21:45 +0530

    updated morover

--- Mars Rover Picture Tool/application.py
+++ Mars Rover Picture Tool/application.py
@@ -10,7 +10,7 @@ class Application(tk.Frame):
 		self.master = master
 		self.grid()
 
-		self.rovers = ['Perseverance', 'Curiosity', 'Oppurtunity', 'Spirit']
+		self.rovers = ['Perseverance', 'Curiosity', 'Opportunity', 'Spirit']
 		self.current_rover = None
 		self.btn_list = []
 
@@ -21,41 +21,72 @@ class Application(tk.Frame):
 		self.curiosity_cam = ['Any Camera', 'FHAZ', 'RHAZ', 'MAST', 'CHEMCAM', 'MAHLI', 'MARDI', 'NAVCAM']
 		self.oppurtunity_cam = ['Any Camera', 'FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES']
 		self.cam_dict = {'Perseverance':self.persevarnce_cam, 'Curiosity':self.curiosity_cam,
-						 'Oppurtunity':self.oppurtunity_cam, 'Spirit':self.oppurtunity_cam}
+						 'Opportunity':self.oppurtunity_cam, 'Spirit':self.oppurtunity_cam}
 		self.current_camera = tk.IntVar()
+
+		self.image_dict = {'Perseverance':Perseverance_pic, 'Curiosity':Curiosity_pic,
+							'Opportunity':Opportunity_pic, 'Spirit':Spirit_pic}
+
+		self.sol_var = tk.StringVar()
+		self.date_var = tk.StringVar()
+		self.latest_var = tk.IntVar()
 
 		self.draw_frames()
 		self.draw_buttons()
+		self.draw_date_frame()
 
 	def draw_frames(self):
-		self.topbar = tk.Frame(self, width=1050, height=50, bg='#252525')
-		self.imgFrame = tk.Frame(self, width=650, height=450, bg='#252525')
-		self.infoFrame =  tk.Frame(self, width=650, height=50, bg='#252525')
-		self.rightlabel = tk.Frame(self, width=400, height=40, bg='#252525')
-		self.rightbar = tk.Frame(self, width=400, height=410, bg='white')
+		self.topbar = tk.Frame(self, width=1050, height=50, bg='#252525', highlightthickness=1)
+		self.imgFrame = tk.Frame(self, width=650, height=450, bg='#252525', highlightthickness=1)
+		self.infoFrame =  tk.Frame(self, width=650, height=50, bg='dodgerblue3')
+		self.rightlabel = tk.Frame(self, width=400, height=40, bg='#252525', highlightthickness=1)
+		self.rightbar1 = tk.Frame(self, width=200, height=410, bg='white')
+		self.rightbar2 = tk.Frame(self, width=200, height=410, bg='white')
 		self.bottomBox = tk.Frame(self, width=400, height=50, bg='dodgerblue3') 
 
-		self.topbar.grid(row=0, column=0, columnspan=2)
+		self.topbar.grid(row=0, column=0, columnspan=3)
 		self.imgFrame.grid(row=1, column=0, rowspan=2)
 		self.infoFrame.grid(row=3, column=0)
-		self.rightlabel.grid(row=1, column=1)
-		self.rightbar.grid(row=2, column=1)
-		self.bottomBox.grid(row=3, column=1)
+		self.rightlabel.grid(row=1, column=1, columnspan=2)
+		self.rightbar1.grid(row=2, column=1)
+		self.rightbar2.grid(row=2, column=2)
+		self.bottomBox.grid(row=3, column=1, columnspan=2)
 
 		self.topbar.grid_propagate(False)
 		self.imgFrame.grid_propagate(False)
 		self.infoFrame.grid_propagate(False)
 		self.rightlabel.grid_propagate(False)
-		self.rightbar.grid_propagate(False)
+		self.rightbar1.grid_propagate(False)
+		self.rightbar2.grid_propagate(False)
 		self.bottomBox.grid_propagate(False)
 
-		self.canvas = tk.Canvas(self.imgFrame, width=650, height = 450, bg='#252525')
-		self.canvas.grid(row=0, column=0, padx=0)
+		self.canvas = tk.Canvas(self.imgFrame, width=650, height = 450, bg='#252525', bd=0, relief=tk.FLAT,
+					highlightthickness=0)
+		self.canvas.grid(row=0, column=0, padx=0, pady=0)
+
+		tk.Label(self.rightlabel, text='Select Camera', bg='#252525', fg='white', font=('veradan', 10, 'bold')
+					).grid(row=0, column=0, pady=5, padx=30)
+		tk.Label(self.rightlabel, text='Select Day', bg='#252525', fg='white', font=('veradan', 10, 'bold')
+					).grid(row=0, column=1, pady=5, padx=90)
+
+	def draw_date_frame(self):
+		ttk.Label(self.rightbar2, text='Enter Sol ( Martian Day )',
+							).grid(row=0, column=0, padx=30, pady=(5,0))
+		self.solEntry = ttk.Entry(self.rightbar2, width=20, textvariable=self.sol_var)
+		self.solEntry.grid(row=1, column=0, padx=30, pady=(5,0))
+
+		ttk.Label(self.rightbar2, text='Enter Earth Date\n( YYYY-MM-DD )',
+						width=20, anchor=tk.CENTER).grid(row=2, column=0, padx=30, pady=(45,0))
+		self.dateEntry = ttk.Entry(self.rightbar2, width=20, textvariable=self.date_var)
+		self.dateEntry.grid(row=3, column=0, padx=30, pady=(5,0))
+
+		self.latest = ttk.Checkbutton(self.rightbar2, text='Get Latest', width=13, variable=self.latest_var)
+		self.latest.grid(row=4, column=0, pady=(45,0), padx=30)
 
 	def draw_buttons(self):
 		cindex = 0
 		for text in self.rovers:
-			btn = tk.Button(self.topbar, text=text, width=25,
+			btn = tk.Button(self.topbar, text=text, width=25, font=('veradan', 10, 'bold'),
 					relief=tk.RAISED, bg='#e84545', fg='white')
 			btn.config(command=partial(self.set_selection, btn, text))
 			btn.grid(row=0, column=cindex, padx=(20,0), pady=10)
@@ -69,35 +100,31 @@ class Application(tk.Frame):
 		self.settings_btn.grid(row=0, column=5, padx=(70,0))
 
 	def set_selection(self, widget, text):
-		# if self.render_id:
-		# 	self.imgFrame.after_cancel(self.render_id)
-		# 	self.render_id = None
-
-		for w in self.imgFrame.winfo_children():
-			w.destroy()
-
-		self.canvas = tk.Canvas(self.imgFrame, width=650, height= 450, bg='#252525')
-		self.canvas.grid(row=0, column=0, sticky='nsew')
-
 		for w in self.topbar.winfo_children()[:4]:
 			w.config(relief=tk.FLAT, bg='#e84545')
 
 		for w in self.bottomBox.winfo_children():
 			w.destroy()
 
-		for w in self.rightbar.winfo_children():
+		for w in self.rightbar1.winfo_children():
 			w.destroy()
 
-		
 		widget.config(relief=tk.RAISED, bg='dodgerblue3')
 		self.current_rover = text
+
+		image = self.image_dict[text]
+		self.canvas.create_image(0, 0, anchor='nw', image=image)
 
 		rover = self.cam_dict[text]
 		rover_cams = {camera : index+1 for index, camera in enumerate(rover)}
 		r = 0
 		for text, value in rover_cams.items():
-			ttk.Radiobutton(self.rightbar, text=text, variable=self.current_camera,
-					value=value, command=self.show_option, width=25).grid(row=r, column=0, padx=10, pady=2)
+			if r == 0:
+				pady=(5,0)
+			else:
+				pady=2
+			ttk.Radiobutton(self.rightbar1, text=text, variable=self.current_camera,
+					value=value, command=self.show_option, width=25).grid(row=r, column=0, padx=15, pady=pady)
 			r += 1
 		self.current_camera.set(1)
 		self.show_option()
@@ -111,10 +138,14 @@ class Application(tk.Frame):
 
 if __name__ == '__main__':
 	root = tk.Tk()
-	root.geometry('1050x550+80+80')
+	root.geometry('1050x550+130+80')
 	root.title('MaRover Picture')
 
 	settings_icon = PhotoImage(file='assets/settings.png')
+	Perseverance_pic = PhotoImage(file='assets/Perseverance_rover.png')
+	Curiosity_pic = PhotoImage(file='assets/Curiosity_rover.png')
+	Opportunity_pic = PhotoImage(file='assets/Opportunity_rover.png')
+	Spirit_pic = PhotoImage(file='assets/Spirit_rover.png')
 
 	app = Application(master=root)
 	app.mainloop()
